10c50f4d0df08de6b3d89f4b3263c4e6d70272c9
Bea <beaest@usal.es>
2021-02-15 20:56:11 +0100

    'commit'

--- src/main/java/com/TfgBeaEst/Controller/OperationsController.java
+++ src/main/java/com/TfgBeaEst/Controller/OperationsController.java
@@ -132,4 +132,88 @@ public class OperationsController {
 		return responseEntity;
 	}
 	
+	
+	@RequestMapping(value="/sacardatos", method=RequestMethod.POST)
+	public ResponseEntity<Map<String,String>> SacarDatos(@RequestBody Usuario usuario) {
+		
+		System.out.println("INICIO de sacar datos del usuario para Ver Perfil");
+		
+		String user = usuario.getUsuario();
+		
+		ResponseEntity<Map<String,String>> responseEntity = null;
+		Map<String,String> result = new HashMap<>();
+		
+		//Consulta a base de datos para comprobar si existe en la tabla usuarios.
+		Connection conexion = null;
+		
+		// Cargar el driver
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			try {
+				conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/tfg_v1", "root", "");
+				
+				Statement s = null;
+				try {
+					s = conexion.createStatement();
+					
+					try {
+			        	//Sacamos todos los datos del usuario conectado
+							ResultSet comp_user = s.executeQuery("SELECT Nombre, PrimerApellido, SegundoApellido, DNINIF, CorreoElectronico FROM usuarios WHERE Usuario='"+user+"'");
+							
+							System.out.println("SELECT * FROM usuarios WHERE Usuario='"+user+"'");
+							
+							//Para obtener los resultados del usuario conectado
+							if(comp_user.next()){
+								String nombre = comp_user.getString("Nombre");
+								String PApellido = comp_user.getString("PrimerApellido");
+								String SApellido = comp_user.getString("SegundoApellido");
+								String DNINIF = comp_user.getString("DNINIF");
+								String CElectronico = comp_user.getString("CorreoElectronico");
+								
+								System.out.println("Nombre: " + nombre + "Apellidos: " + PApellido + " " + SApellido + "DNI: " + DNINIF + "Correo Electrónico: " + CElectronico);
+								
+								result.put("Nombre", nombre);
+								result.put("PApellido", PApellido);
+								result.put("SApellido", SApellido);
+								result.put("DNINIF", DNINIF);
+								result.put("CElectronico", CElectronico);
+							}
+							
+						//Sacar los resultados del número de explotación del usuario
+						
+							
+						responseEntity = new ResponseEntity<>(result, HttpStatus.OK);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						responseEntity = new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+						
+						System.out.println("ERROR al hacer las consultas SQL");
+					}
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					responseEntity = new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+					
+					System.out.println("ERROR al crear el estamento de la consulta sql");
+				}
+			} catch (SQLException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+				responseEntity = new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+				System.out.println("ERROR al hacer la conexión a la base de datos");
+			} 
+			
+		} catch (ClassNotFoundException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+			responseEntity = new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+			System.out.println("ERROR al cargar el driver de sql");
+		}
+		
+		System.out.println("FIN de sacar datos del usuario para Ver Perfil");
+		return responseEntity;
+		
+	}
 }
--- src/main/resources/templates/prueba.js
+++ src/main/resources/templates/prueba.js
@@ -1,9 +1,40 @@
-function error(){
-	 $('body').css('display','block');
-	 var show = sessionStorage.getItem("noLog");
-	 if ( show == "show" ){
-		 $("#noLogin").show();
-	 }else{
-		 $("#noLogin").hide();
-	 }
+function VerPerfil(){
+	//Primero hay que sacar los datos de base de datos de el usuario que se encuentra conectado
+	var usuario = sessionStorage.getItem("usuario_activo");
+	
+	var usuarioDatos= {
+		'usuario' : usuario,
+	};
+
+	var usuarioLoginJSON = JSON.stringify(usuarioDatos);
+
+	var url = './sacardatos';
+
+	$.ajax({
+		type : 'post',
+		url : url,
+		contentType : 'application/json',
+		dataType : 'json',
+		data : usuarioLoginJSON,
+		success : function(data, textStatus, jqXHR) {
+			//Guardar las variables globales para mostrarlas en los input de Ver Perfil
+			sessionStorage.setItem('Nombre', data.Nombre);
+			sessionStorage.setItem('PApellido', data.PApellido);
+			sessionStorage.setItem('SApellido', data.SApellido);
+			sessionStorage.setItem('DNINIF', data.DNINIF);
+			sessionStorage.setItem('CElectronico', data.CElectronico);
+		},
+		error : function(jqXHR, exception) {
+			// mostrarPantallaError(url, jqXHR);
+		},
+		statuscode : {
+			404 : function() {
+				//mostrarPantallaError(url);
+			}
+		}
+	});
+	
+	//Luego redirigir a la página
+	var url = "./VerPerfil.html";
+    window.location.href = url;
 }
